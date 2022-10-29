5f76a5e95cd209dd22c45421689a48ee280ef158
Matheus Tanaka <matheustanakanog@gmail.com>
2022-10-29 18:06:19 -0300

    feat: add dotenv config, connection to database and import routes

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,19 @@
+import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import express, { Request, Response } from "express";
-import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+import { connectDB } from "./database/mongo";
+import { routes } from "./routes/routes"
+
+
+dotenv.config();
 const app = express();
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
+app.use(routes)
 
-const database = async (): Promise<void> => {
-    await mongoose.connect("mongodb://AzureDiamond:hunter2@172.24.0.2:27017/?authSource=admin")
-    .then(() => console.log("Connected to DB"))
-    
-}
-database().catch((e) => console.log(e));
+connectDB().catch(e => console.log(e));
 
 const PORT = process.env.PORT ?? 3000;
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello world")
-})
-
-app.listen(PORT, () => console.log(`Server is running at port http://localhost:${PORT}/`))
+app.listen(PORT, () => console.log(`Server is running at port http://localhost:${PORT}/`));
